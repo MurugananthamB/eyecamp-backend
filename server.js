32e737ea9d49d07586de280e4a7bd74e02faf356
Muruganantham Bharathakrishnan <166166883+MurugananthamB@users.noreply.github.com>
2025-03-09 18:08:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -40,5 +40,6 @@ if (SERVER_URL) {
 }
 
 // ✅ Start Server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+const PORT = process.env.PORT || 10000;
+const server = app.listen(PORT, "0.0.0.0", () =>
+  console.log(`🚀 Server running on port ${PORT}`));
