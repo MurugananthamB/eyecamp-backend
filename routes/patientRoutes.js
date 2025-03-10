d12561b6bab96f5938c20bbae0a22e243f582f48
MurugananthamB <murugananthamanand01212@gmail.com>
2025-03-10 23:46:25 +0530

    fixed all of

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const { submitPatientData } = require("../controllers/patientController");
+const {
+  submitPatientData,
+  getAllPatients,
+  getPatientDetails,
+} = require("../controllers/patientController");
 
 // POST /pts-dts - Submit patient data
 router.post("/", submitPatientData);
+
+// GET /api/patients - Get all patient data
+router.get("/", getAllPatients);
+
+// GET /api/patients/:patientId - Get patient details by ID
+router.get("/:regNo", getPatientDetails);
 
 module.exports = router;
